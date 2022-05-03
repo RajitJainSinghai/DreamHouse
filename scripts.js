6443e69c21b7f83169e380dcc2912a947259d061
Project3techis <project3@techis.io>
2022-05-03 10:40:49 +0530

    added home sale

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
+function openFormForSignIn() {
+  document.getElementById("popupFormSignIn").style.display = "block";
+}
+function closeFormSignIn() {
+  document.getElementById("popupFormSignIn").style.display = "none";
+}
